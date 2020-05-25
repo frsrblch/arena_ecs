5c1ac31167c978c370f68f958602b7e99a3b66c5
Fraser Balch <farseer.ulwithy@gmail.com>
2020-05-25 14:31:45 -0600

    created traits module

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,10 @@ pub use allocator::*;
 pub use arena::*;
 pub use ids::*;
 pub use storage::*;
+pub use traits::*;
 
 mod allocator;
 mod arena;
 mod ids;
 mod storage;
-
-pub trait Create<ROW> {
-    type Id;
-    fn create(&mut self, row: ROW) -> Self::Id;
-}
-
-pub trait CreateLinked<ROW> {
-    type Links;
-    type Id;
-    fn create_linked(&mut self, row: ROW, links: Self::Links) -> Self::Id;
-}
+mod traits;
---
+++ src/traits.rs
@@ -0,0 +1,24 @@
+pub trait Get<ID, T> {
+    fn get(&self, id: ID) -> &T;
+    fn get_mut(&mut self, id: ID) -> &mut T;
+}
+
+pub trait GetOpt<ID, T> {
+    fn get(&self, id: ID) -> Option<&T>;
+    fn get_mut(&mut self, id: ID) -> Option<&mut T>;
+}
+
+pub trait Insert<ID, T> {
+    fn insert(&mut self, id: ID, value: T);
+}
+
+pub trait Create<ROW> {
+    type Id;
+    fn create(&mut self, row: ROW) -> Self::Id;
+}
+
+pub trait CreateLinked<ROW> {
+    type Links;
+    type Id;
+    fn create_linked(&mut self, row: ROW, links: Self::Links) -> Self::Id;
+}
