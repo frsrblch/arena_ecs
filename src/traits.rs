2e0fa48129eac17cfbf6b8dbdd8e0b00ed9609eb
Fraser Balch <farseer.ulwithy@gmail.com>
2020-06-12 19:07:26 -0600

    removed unused Get and Insert traits

--- src/traits.rs
+++ src/traits.rs
@@ -1,14 +1,5 @@
 use crate::*;
 
-pub trait Get<ID, T> {
-    fn get(&self, id: ID) -> Option<&T>;
-    fn get_mut(&mut self, id: ID) -> Option<&mut T>;
-}
-
-pub trait Insert<ID, T> {
-    fn insert(&mut self, id: ID, value: T);
-}
-
 pub trait Indexes<A: Arena>: Copy {
     fn index(self) -> usize;
     fn id(self) -> Id<A>;
